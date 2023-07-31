a9bd96714d2b440809d51a0ab6b742a48d5f7f94
baps-reidelas2014 <gustavo-baps@educar.rs.gov.br>
2023-07-31 08:54:41 -0300

    commit 31/07

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ document.addEventListener("DOMContentLoaded", function(){
     var mensagens = document.getElementById('mensagens');
 mensagens.scrollTop = mensagens.scrollHeight;
 });
+var popupVisible = false;
+function mostrarPopup(){
+    var popup = document.getElementById("popup");
+    popupVisible = !popupVisible;
+    popup.style.display = popupVisible ? "block" : "none";
+}
+
+function esconderPopup(){
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
